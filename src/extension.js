1a3a14600e6a7d9d1829ae1e302b76b4dc8d7f2b
eugene-rom <eros2@inbox.ru>
2023-09-26 11:53:53 +0300

    gnome-shell 45 compatibility, drop compat with previous versions

--- src/extension.js
+++ src/extension.js
@@ -1,12 +1,17 @@
 /* global imports */
 
-const { GLib, Gio, GObject, St, Shell } = imports.gi;
+import GLib from 'gi://GLib'
+import Gio from 'gi://Gio'
+import GObject from 'gi://GObject'
+import St from 'gi://St'
+import Shell from 'gi://Shell'
 
-const Main = imports.ui.main;
-const Panel = imports.ui.panel;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Panel from 'resource:///org/gnome/shell/ui/panel.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const ICON_SIZE    = 26;
 const COL_IDLE     = 3;
@@ -15,16 +20,18 @@ const TEXT_SYSMON  = 'System Monitor...';
 const TEXT_CPU     = 'CPU: ';
 const TEXT_LOGID   = 'syspeek-gs';
 
-let syspeek;
 let enabled = false;
 let sourceId = null;
 
-let SysPeekGS = GObject.registerClass(
-class SysPeekGS extends PanelMenu.Button
+class SysPeekGSBtn extends PanelMenu.Button
 {
-    _init()
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor( path )
     {
-        super._init( 0.0, TEXT_SYSPEEK );
+        super( 0.0, TEXT_SYSPEEK );
 
         this._last_total = 0;
         this._last_busy = 0;
@@ -34,7 +41,7 @@ class SysPeekGS extends PanelMenu.Button
 
         this._icons = [];
         for ( let i = 0; i <= 100; i += 10 ) {
-            let gicon = Gio.icon_new_for_string( Me.path + '/icons/syspeek-' + i + '.svg' );
+            let gicon = Gio.icon_new_for_string( path + '/icons/syspeek-' + i + '.svg' );
             this._icons.push( new St.Icon( { gicon: gicon, icon_size: ICON_SIZE } ) );
         }
 
@@ -51,82 +58,88 @@ class SysPeekGS extends PanelMenu.Button
         this._micpu = new PopupMenu.PopupMenuItem( TEXT_CPU );
         this.menu.addMenuItem( this._micpu );
 
-        this.destroy = () => {
-            this._input.close(null);
-            super.destroy();
-        };
-
-        this._update = ( percentage ) => {
-            if ( enabled ) {
-                let icon_old = this._hbox.get_child_at_index( 0 );
-                let icon_new = this._icons[ Math.trunc(percentage / 10) ];
-                if ( icon_new !== icon_old ) {
-                    this._hbox.replace_child( icon_old, icon_new );
-                }
-                this._micpu.label.set_text( TEXT_CPU + percentage.toFixed(1) + '%' );
-            }
-        };
-
-        this._convert_string = ( line ) => {
-            let a = line.split(' ');
-            a = a.filter( n => { return n !== ''; } );
-            a.shift();
-            a.splice(7, 3);
-            return a.map(Number);
-        };
-
-        this._read_stat = () => {
-            if ( enabled )
-            {
-                this._input.seek( 0, GLib.SeekType.SET, null );
-                let [line, length] = this._input.read_line_utf8(null);
-
-                if (line === null) {
-                    return;
-                }
-
-                //global.log( TEXT_LOGID, 'Line: ' + line );
-                let stats = this._convert_string( line );
-                let total = stats.reduce( (a, b) => a + b, 0 );
-                let busy = total - stats[ COL_IDLE ];
-
-                let delta_total = total - this._last_total;
-                let delta_busy = busy - this._last_busy;
-
-                let percentage = 0;
-                if ( ( delta_total > 0 ) && ( delta_busy > 0 ) ) {
-                    percentage = (delta_busy / delta_total) * 100;
-                }
-
-                this._update(percentage);
-
-                this._last_total = total;
-                this._last_busy = busy;
-            }
-
-            return enabled;
-        };
-
         sourceId = GLib.timeout_add_seconds( GLib.PRIORITY_DEFAULT, 1, this._read_stat.bind(this) );
     }
-} );
 
-
-function enable() {
-    enabled = true;
-    syspeek = new SysPeekGS;
-    Main.panel.addToStatusArea( TEXT_SYSPEEK, syspeek );
-}
-
-function disable()
-{
-    enabled = false;
- 
-    if ( sourceId ) {
-        GLib.Source.remove( sourceId );
-        sourceId = null;
+    destroy() {
+        this._input.close(null);
+        super.destroy();
     }
 
-    syspeek.destroy();
-    syspeek = null;
+    _update( percentage ) 
+    {
+        if ( enabled ) {
+            let icon_old = this._hbox.get_child_at_index( 0 );
+            let icon_new = this._icons[ Math.trunc(percentage / 10) ];
+            if ( icon_new !== icon_old ) {
+                this._hbox.replace_child( icon_old, icon_new );
+            }
+            this._micpu.label.set_text( TEXT_CPU + percentage.toFixed(1) + '%' );
+        }
+    }
+
+    _convert_string( line )
+    {
+        let a = line.split(' ');
+        a = a.filter( n => { return n !== ''; } );
+        a.shift();
+        a.splice(7, 3);
+        return a.map(Number);
+    }
+
+    _read_stat()
+    {
+        if ( enabled )
+        {
+            this._input.seek( 0, GLib.SeekType.SET, null );
+            let [line, length] = this._input.read_line_utf8(null);
+
+            if (line === null) {
+                return;
+            }
+
+            //global.log( TEXT_LOGID, 'Line: ' + line );
+            let stats = this._convert_string( line );
+            let total = stats.reduce( (a, b) => a + b, 0 );
+            let busy = total - stats[ COL_IDLE ];
+
+            let delta_total = total - this._last_total;
+            let delta_busy = busy - this._last_busy;
+
+            let percentage = 0;
+            if ( ( delta_total > 0 ) && ( delta_busy > 0 ) ) {
+                percentage = (delta_busy / delta_total) * 100;
+            }
+
+            this._update(percentage);
+
+            this._last_total = total;
+            this._last_busy = busy;
+        }
+
+        return enabled;
+    }
 }
+
+export default class SysPeekGS extends Extension 
+{
+    enable() {
+        enabled = true;
+        this._syspeek = new SysPeekGSBtn( this.path );
+        Main.panel.addToStatusArea( TEXT_SYSPEEK, this._syspeek );
+    }
+
+    disable() 
+    {
+        enabled = false;
+
+        if ( sourceId ) {
+            GLib.Source.remove( sourceId );
+            sourceId = null;
+        }
+
+        this._syspeek.destroy();
+        this._syspeek = null;
+    }
+}
+
